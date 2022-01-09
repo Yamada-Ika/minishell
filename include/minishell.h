fd64f2dfe7fc1e2ef5c39a60473aa55197dc3c9e
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-09 19:25:49 +0900

    [Add]token type

--- include/minishell.h
+++ include/minishell.h
@@ -11,7 +11,25 @@
 #include <readline/history.h>
 
 #include "../libft/libft.h"
+#include <stdbool.h>
 
 char	***ft_split_triple(char const **strs, char *sets);
+
+typedef enum e_token_kind
+{
+	TK_RESERVED, // 予約語
+	// TK_META,     // メタ文字 (\, ...)
+	TK_PIPE,     // パイプ
+	// TK_COMMAND,  // コマンドトークン
+	TK_STRING,   // 文字列(コマンド、コマンドの引数、etc)
+	TK_EOF,      // 入力の終わりを表すトークン
+}	t_token_kind;
+
+typedef struct s_token
+{
+	t_token_kind	kind;
+	struct s_token	*next;
+	char			*str;
+}	t_token;
 
 # endif
