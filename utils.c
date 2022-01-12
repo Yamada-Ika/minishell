8623f287e7651ceb4fe4f0b08b059c6972edf14a
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-13 03:37:18 +0900

    fix: check_op

--- utils.c
+++ utils.c
@@ -5,3 +5,18 @@ void error(char *str)
     ft_putstr_fd(str, 2);
     exit(1);
 }
+
+int	check_op(t_token *tok)
+{
+	const char *kw[] = {"<<", ">>", "<", ">", "|", "'", "\"", NULL};
+	size_t  i;
+
+	i = 0;
+	while (kw[i] != NULL)
+	{
+		if (!ft_strncmp(tok->str, kw[i], ft_strlen(kw[i])))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
