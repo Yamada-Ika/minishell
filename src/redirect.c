d06b8286ad77cf29c8fda7018878bba4a1efba6f
shige <shigebask@gmail.com>
2022-01-18 02:16:33 +0900

    fix: modified open option

--- src/redirect.c
+++ src/redirect.c
@@ -6,9 +6,9 @@ void	handle_in_redir(t_redirect_list *redirect)
 
 	fprintf(stderr,"handle_in_redir called:  \n");
 	if (ft_strncmp(redirect->redirect, ">>", 2) == 0)
-		fd = open(redirect->word, O_RDWR | O_CREAT, S_IREAD | S_IWRITE);
+		fd = open(redirect->word, O_RDWR | O_APPEND | O_CREAT, 0664);
 	else
-		fd = open(redirect->word, O_RDWR | O_CREAT | O_TRUNC, S_IREAD | S_IWRITE);
+		fd = open(redirect->word, O_RDWR | O_CREAT | O_TRUNC, 0664);
 	if (fd == -1)
 		error(strerror(errno));
 	dup2(fd, 1);
@@ -28,5 +28,5 @@ void	handle_out_redir(t_redirect_list *redirect)
 	dup2(fd, 0);
 	close(fd);
 	if (redirect->next != NULL)
-		handle_in_redir(redirect->next);
+		handle_out_redir(redirect->next);
 }
