4cb3c9c9dc6377475b59d3ca52bc24e985cc5d9d
shige <shigebask@gmail.com>
2022-01-18 17:33:14 +0900

    fix:　add get_here_doc

--- src/here_doc.c
+++ src/here_doc.c
@@ -1,6 +1,20 @@
 #include "minishell.h"
 
-char	*here_doc(char *eos)
+//void	get_here_doc(char **)
+//{
+//
+//}
+
+bool	last_is_here_doc(t_redirect_list *last)
+{
+	if (last == NULL)
+		return (false);
+	if (ft_strncmp(last->redirect, "<<", 2) == 0)
+		return (true);
+	return (false);
+}
+
+char	*get_here_doc(char *eos)
 {
 	char	*str;
 	char	*doc;
@@ -8,6 +22,7 @@ char	*here_doc(char *eos)
 	size_t eos_len;
 
 	eos_n = ft_strjoin(eos, "\n");
+	free(eos);
 	eos_len = ft_strlen(eos_n);
 	doc = ft_strdup("");
 	if (doc == NULL || eos_n == NULL)
@@ -18,15 +33,14 @@ char	*here_doc(char *eos)
 		str =  get_next_line(0);
 		if (str == NULL)
 			error("malloc error");
-		if (ft_strncmp(str, eos_n, eos_len + 1) == 0)
-		{
-			free(str);
+		if (ft_strncmp(str, eos_n, eos_len) == 0)
 			break;
-		}
 		doc = ft_strjoin_with_free(&doc, &str);
 		if (doc == NULL)
 			error("malloc error");
 	}
+	free(str);
+	free(eos_n);
 	return (doc);
 }
 
