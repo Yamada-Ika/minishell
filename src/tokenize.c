9cf88b7401c77ec2bf4bfdd35bbb8ad4cee09e13
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-18 15:15:05 +0900

    remove: main_

--- src/tokenize.c
+++ src/tokenize.c
@@ -102,31 +102,3 @@ t_token *tokenize(char *p)
 	head.next->prev = cur;
 	return (head.next);
 }
-//int	main(int argc, char **argv)
-t_node	*main_(char *str)
-{
-//	if (argc == 1)
-//		error("Invalid argument!\n");
-	t_token	*token = tokenize(str);
-
-	// tokenize
-	if (token->kind == TK_EOF)
-	{
-		free(token);
-		return (0);
-	}
-	printf("%s\n", token->prev->prev->str);
-	debug_tokenize(token);
-	// printf("%.*s\n", token->len, token->str);
-
-	// parse
-	t_node	*node = command_line(&token);
-	printf("parse: =========================================\n");
-	debug_node(node);
-	 expansion(node);
-	 printf("expansion: =========================================\n");
-	 debug_node(node);
-//	 printf("tokenize ========= token %zu\n", node->word_list_size);
-//	 printf("tokenize ========= token %s\n", node->word_list->str);
-	 return (node);
-}
