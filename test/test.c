c1656e0309ce2872de62d7035bf1cea0ebc1f0f2
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-18 16:08:22 +0900

    fix: test

--- test/test.c
+++ test/test.c
@@ -1,46 +1,48 @@
 #include "minishell.h"
 
-
-//int	main(int argc, char **argv)
-t_node	*main_(char *str)
-{
-//	if (argc == 1)
-//		error("Invalid argument!\n");
-	t_token	*token = tokenize(str);
-
-	// tokenize
-	if (token->kind == TK_EOF)
-	{
-		free(token);
-		return (0);
-	}
-	printf("%s\n", token->prev->prev->str);
-	debug_tokenize(token);
-	// printf("%.*s\n", token->len, token->str);
-
-	// parse
-	t_node	*node = command_line(&token);
-	printf("parse: =========================================\n");
-	debug_node(node);
-	 expansion(node);
-	 printf("expansion: =========================================\n");
-	 debug_node(node);
-//	 printf("tokenize ========= token %zu\n", node->word_list_size);
-//	 printf("tokenize ========= token %s\n", node->word_list->str);
-	 return (node);
-}
+#ifdef ALL
+#define TOKEN 1
+#define PARSE 1
+#define EXPAND 1
+#endif
 
 int	main(int argc, char **argv, char **envp)
 {
+	char	*str;
+	t_token	*token;
 	t_node	*node;
-	char **paths;
+	char	**paths;
 
 	if (argc == 1)
-	error("Invalid argument!\n");
-	node = main_(argv[1]);
-	paths = get_command_path(envp);
-	printf("exec.c =========================================\n");
-	handle_command(paths, node);
+		error("Invalid argument!\n");
+	str = argv[1];
+
+	// tokenize
+	token = tokenize(str);
+
+	#ifdef TOKEN
+	printf("tokenize =========================================\n");
+	debug_tokenize(token);
+	#endif
+
+	// parse
+	node = command_line(&token);
+	#ifdef PARSE
+	printf("parse =========================================\n");
+	debug_node(node);
+	#endif
+
+	// expansion
+	expansion(node);
+	#ifdef EXPAND
+	printf("expand =========================================\n");
+	debug_node(node);
+	#endif
+
+	// handle_command
+	// printf("exec =========================================\n");
+	// paths = get_command_path(envp);
+	// handle_command(paths, node);
 //	system("leaks minishell");
 	exit (0);
 }
