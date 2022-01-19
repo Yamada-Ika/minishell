b5fb343bf8273ef880d5835259107e5a0eb59bbb
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-19 22:33:13 +0900

    add: instead of getenv

---
+++ builtin/my_getenv.c
@@ -0,0 +1,24 @@
+#include "envvar.h"
+
+// keyが見つかったらstrdupしてvalを返す
+// そうしたら、double freeを防げる気がする
+// いやどうだろ、あえてしない方が関数外でfreeする必要ないし良いかも
+char	*my_getenv(t_envvar *envs, char *key)
+{
+	char	*val;
+
+	if (envs == NULL)
+		return (NULL);
+	while (envs != NULL)
+	{
+		if (ft_strcmp(key, envs->key) == 0)
+		{
+			val = ft_strdup(envs->val);
+			if (val == NULL)
+				exit(1);
+			return (val);
+		}
+		envs = envs->next;
+	}
+	return (NULL);
+}
