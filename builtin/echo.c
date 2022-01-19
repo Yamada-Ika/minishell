f5c4121e5eafd2cb870c318a0777db8da8988c1e
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-19 22:33:29 +0900

    add: echo

--- builtin/echo.c
+++ builtin/echo.c
@@ -1,1 +1,30 @@
-#include <stdio.h>
+#include "builtin.h"
+
+static bool	_get_option(char **args)
+{
+	if (ft_strcmp(args[0], "-n") == 0)
+		return (true);
+	return (false);
+}
+
+void	echo(char **args)
+{
+	bool	flag_n;
+	size_t	i;
+	char	*s;
+
+	flag_n = _get_option(args);
+	i = 0;
+	if (flag_n)
+		i = 1;
+	while (args[i])
+	{
+		printf("%s", args[i]);
+		if (args[i + 1] == NULL)
+			break ;
+		printf(" ");
+		i++;
+	}
+	if (flag_n)
+		printf("\n");
+}
