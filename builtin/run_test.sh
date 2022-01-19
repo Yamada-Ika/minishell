ce94f37d531a4d15e54c772635f2adaecad1b787
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-19 22:37:10 +0900

    add: run test program script

--- builtin/run_test.sh
+++ builtin/run_test.sh
@@ -11,8 +11,17 @@ then
 elif [ $test_cmd_name = "unset" ]
 then
 	SRCS="unset_test.c unset.c export.c env.c envvar_utils.c"
+elif [ $test_cmd_name = "getenv" ]
+then
+	SRCS="my_getenv_test.c my_getenv.c export.c env.c inherite_env_val.c envvar_utils.c"
+elif [ $test_cmd_name = "echo" ]
+then
+	SRCS="echo_test.c echo.c"
+elif [ $test_cmd_name = "inherite" ]
+then
+	SRCS="inherite_env_val_test.c inherite_env_val.c env.c export.c envvar_utils.c"
 fi
 
-gcc $SRCS -o test
+gcc $SRCS -o test -I../libft ../libft/libft.a
 ./test
 rm -rf ./test
