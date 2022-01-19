32d30b8faa8d67b7c02cd0dd7a10190ad41fb213
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-19 22:35:24 +0900

    add: get env var from envp**

---
+++ builtin/inherite_env_val.c
@@ -0,0 +1,21 @@
+#include "envvar.h"
+
+void	inherite_env_val(t_envvar **env_list, char **envp)
+{
+	size_t	i;
+	size_t	key_len;
+	char	*key;
+	char	*val;
+
+	if (env_list == NULL)
+		exit(1);
+	i = 0;
+	while (envp[i] != NULL)
+	{
+		key_len = ft_strchr(envp[i], '=') - envp[i];
+		key = ft_substr(envp[i], 0, key_len);
+		val = ft_substr(envp[i], key_len + 1, ft_strlen(envp[i]));
+		msh_export(env_list, key, val);
+		i++;
+	}
+}
