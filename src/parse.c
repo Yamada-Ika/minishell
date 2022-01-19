54ebf37cd2a435121b4475af8416c01e0fc00a6e
shige <shigebask@gmail.com>
2022-01-19 23:09:06 +0900

    fix: free all when parse error happened

--- src/parse.c
+++ src/parse.c
@@ -77,25 +77,35 @@ t_node	*new_node_pipe(t_token *token, t_node *left, t_node *right)
 	return (node);
 }
 
+t_node	*parse_error(t_node *node, t_token *token)
+{
+	free_token_list(token);
+	if (node != NULL)
+		free_node_list(node);
+	return (NULL);
+}
+
 t_node	*command_line(t_token **tok)
 {
-	t_node	*node;
 	t_token	*tmp_tk;
+	t_token	*tk_head;
+	t_node	*node;
 	t_node	*tmp_nd;
 
+	tk_head = *tok;
 	node = new_node_command(tok);
+	if (node == NULL)
+		return (parse_error(node, tk_head));
 	while ((*tok)->kind == TK_OP_PIPE)
 	{
 		tmp_tk = *tok;
 		*tok = (*tok)->next;
 		tmp_nd = new_node_command(tok);
 		if (tmp_nd == NULL)
-		{
-			free_node_list(node);
-			return (NULL);
-		}
+			return (parse_error(node, tk_head));
 		node = new_node_pipe(tmp_tk, node, tmp_nd);
 	}
+	*tok = tk_head;
 	return (node);
 }
 
