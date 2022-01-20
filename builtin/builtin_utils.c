a6bb527c6804dd494ea19958ba1881a00f2dec9f
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-20 15:18:08 +0900

    add: print error message in env

--- builtin/builtin_utils.c
+++ builtin/builtin_utils.c
@@ -9,13 +9,29 @@ void	print_command_usage(char *name, char *usage)
 	ft_putendl_fd(usage, STDERR_FILENO);
 }
 
+void	print_command_usage2(char *name, char *usage)
+{
+	ft_putstr_fd("usage: ", STDERR_FILENO);
+	ft_putstr_fd(name, STDERR_FILENO);
+	ft_putstr_fd(" ", STDERR_FILENO);
+	ft_putendl_fd(usage, STDERR_FILENO);
+}
+
 void	error_option(char *builtin_name, char *option_name)
 {
 	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	ft_putstr_fd(builtin_name, STDERR_FILENO);
 	ft_putstr_fd(": ", STDERR_FILENO);
 	ft_putstr_fd(option_name, STDERR_FILENO);
-	ft_putstr_fd(": invalid option\n", STDERR_FILENO);
+	ft_putendl_fd(": invalid option", STDERR_FILENO);
+}
+
+void	error_option2(char *builtin_name, char option)
+{
+	ft_putstr_fd(builtin_name, STDERR_FILENO);
+	ft_putstr_fd(": illegal option -- ", STDERR_FILENO);
+	ft_putchar_fd(option, STDERR_FILENO);
+	ft_putchar_fd('\n', STDERR_FILENO);
 }
 
 void	error_ident(char *builtin_name, char *ident)
@@ -26,5 +42,5 @@ void	error_ident(char *builtin_name, char *ident)
 	ft_putchar_fd('`', STDERR_FILENO);
 	ft_putstr_fd(ident, STDERR_FILENO);
 	ft_putchar_fd('\'', STDERR_FILENO);
-	ft_putstr_fd(": not a valid identifier\n", STDERR_FILENO);
+	ft_putendl_fd(": not a valid identifier", STDERR_FILENO);
 }
