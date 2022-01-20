af644d57d80cee0a399daf5bcf6c2f8793ba3164
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-20 14:42:31 +0900

    add: argument analyze func

--- builtin/unset.c
+++ builtin/unset.c
@@ -1,4 +1,37 @@
-#include "envvar.h"
+#include "minishell.h"
+
+void	unset_(char **args)
+{
+	size_t	i;
+	char	*equal_at;
+
+	if (args[0] == NULL)
+		return ;
+	i = 0;
+	while (args[i] != NULL)
+	{
+		if (args[0][0] == '-') // is_option
+		{
+			error_option("unset", args[i]);
+			print_command_usage("unset", "unset [name[=value] ...]");
+			i++;
+			continue ;
+		}
+		equal_at = ft_strchr(args[i], '=');
+		if (!ft_isalpha(args[i][0]) || equal_at != NULL)
+		{
+			error_ident("unset", args[i]);
+			i++;
+			continue ;
+		}
+		else
+		{
+			msh_unset(&(g_mshell->envlist), args[i]);
+			i++;
+			continue ;
+		}
+	}
+}
 
 void	msh_unset(t_envvar **envs, char *key)
 {
