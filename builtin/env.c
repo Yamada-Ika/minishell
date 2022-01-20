2fee686b8ebe7668e9353ae84e059a9da1293999
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-20 15:19:01 +0900

    WIP: behavior is defferent when mulit-arguments

--- builtin/env.c
+++ builtin/env.c
@@ -1,4 +1,27 @@
-#include "envvar.h"
+#include "minishell.h"
+
+void	env_(char **args)
+{
+	size_t	i;
+	char	*equal_at;
+
+	if (args[0] == NULL)
+	{
+		msh_env(g_mshell->envlist);
+		return ;
+	}
+	i = 0;
+	while (args[i] != NULL)
+	{
+		if (args[0][0] == '-' && ft_strlen(args[0]) >= 2) // is_option
+		{
+			error_option2("env", args[i][1]);
+			print_command_usage2("env", "env [name=value ...]");
+			return ;
+		}
+		i++;
+	}
+}
 
 void	msh_env(t_envvar *envs)
 {
