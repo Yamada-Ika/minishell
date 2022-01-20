51fb1680dc312128868906ac8b6bf5b63bcf570b
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-20 12:15:24 +0900

    add: support func in export

--- builtin/envvar.h
+++ builtin/envvar.h
@@ -36,5 +36,7 @@ void		add_envlist(t_envvar *olds, t_envvar *new);
 t_envvar	*get_envlistlast(t_envvar *envs);
 void		delete_envvar_contents(char *key, char *val);
 void		delete_envlists(t_envvar *env);
+t_envvar	*get_envvar_with_key(t_envvar *envlist, char *key);
+void		replace_val(t_envvar *env, char *val);
 
 #endif
