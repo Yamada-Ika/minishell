0e703d9a739fa34058f2d2c1e33b650803b440d4
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-20 12:16:52 +0900

    fix: Can work echo, cd, pwd, export

--- src/built_in_command.c
+++ src/built_in_command.c
@@ -10,32 +10,44 @@ void redirect_in_parrent( t_command redir, int *fd)
 		handle_out_redir(redir.out_redir);
 }
 
+// cmds[0]にはコマンド名が入っている
+static void	_exec_builtin_cmd(t_builtin_kind kind, char **cmds)
+{
+	if (kind == BUILTIN_ECHO)
+		echo(++cmds);
+	if (kind == BUILTIN_CD)
+		cd_(cmds);
+	if (kind == BUILTIN_PWD)
+		pwd_();
+	if (kind == BUILTIN_EXPORT)
+		export_(++cmds);
+}
+
 bool	is_exec_built_in(char **cmds, t_command redir)
 {
+	const void	*builtin[] = {
+		"echo", "cd", "pwd", "export", NULL};
+		// "echo", "cd", "pwd", "export", "unset", "env", "exit", NULL};
 	int	fd[2];
+	int	i;
 
 	fprintf(stderr, "is_exec_built_in called\n");
 	if (cmds == NULL)
 		error("error: is_exec_built_in");
-	if (ft_strncmp(cmds[0], "cd", 2) == 0)
+	i = 0;
+	while(builtin[i] != NULL)
 	{
-		redirect_in_parrent(redir, fd);
-		cd_(cmds);
-		if (fd[0])
-			dup2(fd[0], 0);
-		if (fd[1])
-			dup2(fd[1], 1);
-		return (true);
-	}
-	if (ft_strncmp(cmds[0], "pwd", 3) == 0)
-	{
-		redirect_in_parrent(redir, fd);
-		pwd_();
-		if (fd[0])
-			dup2(fd[0], 0);
-		if (fd[1])
-			dup2(fd[1], 1);
-		return (true);
+		if (ft_strcmp(cmds[0], builtin[i]) == 0)
+		{
+			redirect_in_parrent(redir, fd);
+			_exec_builtin_cmd(i, cmds);
+			if (fd[0])
+				dup2(fd[0], 0);
+			if (fd[1])
+				dup2(fd[1], 1);
+			return (true);
+		}
+		i++;
 	}
 	return (false);
 }
