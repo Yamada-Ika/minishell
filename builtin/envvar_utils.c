2e8ec3f4cde26c1816d5366294cfa07adda3449d
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-20 12:14:57 +0900

    fix:

--- builtin/envvar_utils.c
+++ builtin/envvar_utils.c
@@ -7,8 +7,10 @@ t_envvar	*new_envlist(char *key, char *val)
 	new = (t_envvar *)calloc(1, sizeof(t_envvar));
 	if (new == NULL)
 		return (NULL);
-	new->key = key;
-	new->val = val;
+	new->key = ft_strdup(key);
+	new->val = ft_strdup(val);
+	if (new->key == NULL || new->val == NULL)
+		exit(1);
 	return (new);
 }
 
@@ -43,14 +45,47 @@ void	delete_envvar(t_envvar *env)
 	free(env);
 }
 
-void	delete_envlists(t_envvar *env)
+void	delete_envlists(t_envvar *envlist)
 {
 	t_envvar	*cur;
 
-	while (env != NULL)
+	while (envlist != NULL)
 	{
-		cur = env;
-		env = env->next;
+		cur = envlist;
+		envlist = envlist->next;
 		delete_envvar(cur);
 	}
+}
+
+bool	is_exist_key(t_envvar *envlist, char *key)
+{
+	if (envlist == NULL || key == NULL)
+		return (false);
+	while (envlist != NULL)
+	{
+		if (ft_strcmp(envlist->key, key) == 0)
+			return (true);
+		envlist = envlist->next;
+	}
+	return (false);
+}
+
+t_envvar	*get_envvar_with_key(t_envvar *envlist, char *key)
+{
+	if (envlist == NULL || key == NULL)
+		return (NULL);
+	while (envlist != NULL)
+	{
+		if (ft_strcmp(envlist->key, key) == 0)
+			return (envlist);
+		envlist = envlist->next;
+	}
+	return (NULL);
+}
+
+void	replace_val(t_envvar *env, char *val)
+{
+	env->val = ft_strdup(val);
+	if (env->val == NULL)
+		exit(1);
 }
