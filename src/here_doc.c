310bbff79a6e5c67d0a54a7a21b5a881e9090b61
shige <shigebask@gmail.com>
2022-01-21 23:08:35 +0900

    fix: add funcs

--- src/here_doc.c
+++ src/here_doc.c
@@ -16,32 +16,56 @@
 
 char	*get_here_doc(char *eos)
 {
-	char	*str;
 	char	*doc;
-	char	*eos_n;
 	size_t eos_len;
+	char	*line;
+	char	*tmp;
 
-	eos_n = ft_strjoin(eos, "\n");
-	free(eos);
-	eos_len = ft_strlen(eos_n);
 	doc = ft_strdup("");
-	if (doc == NULL || eos_n == NULL)
+	if (doc == NULL)
 		error("malloc error");
+	signal(SIGINT, (void *)interrupt);
 	while (1)
 	{
-		write(0, "heredoc>", 8);
-		str =  get_next_line(0);
-		if (str == NULL)
-			error("malloc error");
-		if (ft_strncmp(str, eos_n, eos_len) == 0)
+		if (g_mshell->interrupt == true)
+			return (doc);
+		line = readline("heredoc> ");
+		if (ft_strcmp(line, eos) == 0)
 			break;
-		doc = ft_strjoin_with_free(&doc, &str);
+		tmp = ft_strdup("\n");
+		line = ft_strjoin_with_free(&line, &tmp);
+		doc = ft_strjoin_with_free(&doc, &line);
 		if (doc == NULL)
 			error("malloc error");
 	}
-	free(str);
-	free(eos_n);
+	free(line);
 	return (doc);
+}
+
+void	get_here_docs(t_redirect_list *redirect)
+{
+	while (redirect)
+	{
+		if (ft_strncmp(redirect->redirect, "<<", 2) == 0)
+			redirect->word = get_here_doc(redirect->word);
+		if (g_mshell->interrupt == true)
+			return;
+		redirect = redirect->next;
+	}
+}
+
+void	get_here_doc_form_each_node(t_node *node)
+{
+	t_node	*left_node;
+
+	while (node->left != NULL)
+	{
+		get_here_docs(node->right->command.out_redir);
+		if (g_mshell->interrupt == true)
+			return;
+		node = node->left;
+	}
+	get_here_docs(node->command.out_redir);
 }
 
 // #include <stdio.h>
