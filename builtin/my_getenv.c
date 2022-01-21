9d4fb24ed147dd4a48faa26e3abc7a2997fa1ecb
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-21 12:19:34 +0900

    fix: do not allocate string

--- builtin/my_getenv.c
+++ builtin/my_getenv.c
@@ -1,8 +1,5 @@
 #include "minishell.h"
 
-// keyが見つかったらstrdupしてvalを返す
-// そうしたら、double freeを防げる気がする
-// いやどうだろ、あえてしない方が関数外でfreeする必要ないし良いかも
 char	*my_getenv(t_envvar *envs, char *key)
 {
 	char	*val;
@@ -12,12 +9,7 @@ char	*my_getenv(t_envvar *envs, char *key)
 	while (envs != NULL)
 	{
 		if (ft_strcmp(key, envs->key) == 0)
-		{
-			val = ft_strdup(envs->val);
-			if (val == NULL)
-				exit(1);
-			return (val);
-		}
+			return (envs->val);
 		envs = envs->next;
 	}
 	return (NULL);
