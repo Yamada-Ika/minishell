a9293c2a0410b81507ca626d3c22b3a5de6ab869
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-22 23:56:03 +0900

    fix

--- src/run_command_line.c
+++ src/run_command_line.c
@@ -8,17 +8,17 @@ void	run_command_line(char *cmd_line)
 	fprintf(stderr, "run_command_line 7: cmd_line = %s\n", cmd_line);
 	token = tokenize(cmd_line);
 	if (token == NULL)
-		return;
+		return ;
 	debug_tokenize(token);
 	node = command_line(&token);
 	if (node == NULL)
-		return;
-	printf("parse    : =========================================\n");
+		return ;
+	fprintf(stderr, "parse    : =========================================\n");
 	debug_node(node);
 	expansion(node);
-	printf("expansion: =========================================\n");
+	fprintf(stderr, "expansion: =========================================\n");
 	debug_node(node);
-	printf("run_command_line 21:  node-word_list-str = %s, kind = %d\n", node->word_list->str, node->word_list->kind);
+	fprintf(stderr, "run_command_line 21:  node-word_list-str = %s, kind = %d\n", node->word_list->str, node->word_list->kind);
 	if (node->word_list->kind != TK_EOF)
 		handle_command(node);
 	free_token_list(token->next);
