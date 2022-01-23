d3f358e2d01425b8aed695b38713b41cb5da1c02
shige <shigebask@gmail.com>
2022-01-24 00:17:10 +0900

    fix: refactor

--- src/exec/exec_utils.c
+++ src/exec/exec_utils.c
@@ -17,3 +17,31 @@ char	**_alloc_word_list(t_token *tok, size_t size)
 	fprintf(stderr, "***word_list_size : %zu\n", word_list_size);
 	return ((char **)ft_calloc(word_list_size + 1, sizeof(char *)));
 }
+
+char	**get_command_path(t_envvar *envlist)
+{
+	char	*val_with_key;
+	char	*path_line;
+	char	**paths;
+
+	val_with_key = my_getenv(envlist, "PATH");
+	if (val_with_key == NULL)
+		return (NULL);
+	errno = INIT_ERRNO;
+	path_line = ft_strdup(val_with_key);
+	if (errno != INIT_ERRNO && path_line == NULL)
+		error(strerror(errno));
+	paths = ft_split(path_line, ':');
+	if (errno != INIT_ERRNO && paths == NULL)
+		error(strerror(errno));
+	free(path_line);
+	return (paths);
+}
+
+void	handle_fd(int close_fd, int dup_fd, int fd)
+{
+	close(close_fd);
+	if (dup2(dup_fd, fd) == ERROR)
+		error(strerror(errno));
+	close(dup_fd);
+}
