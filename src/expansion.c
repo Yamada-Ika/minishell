11e77222d83a383f31923a896b1058be31de811f
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-23 13:59:47 +0900

    fix: comment out

--- src/expansion.c
+++ src/expansion.c
@@ -43,29 +43,29 @@ char	*expand_str(char *str)
 
 	i = 0;
 	t_str = ft_strdup(str);
-	printf("strdup : t_str = %s\n", t_str);
+	// printf("strdup : t_str = %s\n", t_str);
 	while (str[i])
 	{
-		printf("handle_token_in_quotes 52: i = %zu,   str[i] = %c\n", i, str[i]);
+		// printf("handle_token_in_quotes 52: i = %zu,   str[i] = %c\n", i, str[i]);
 		if (str[i] == '$' && (ft_isalnum((unsigned char)(str[i + 1])) || str[i + 1] == '?'))
 		{
 			variable_name = ft_substr(str + i, 1, get_valiable_name_len(str + i + 1));
-			fprintf(stderr, "++++++ variable_name = %s\n", variable_name);
-			fprintf(stderr, "++++++ ? = %s\n", my_getenv(g_mshell->envlist, variable_name));
+			// fprintf(stderr, "++++++ variable_name = %s\n", variable_name);
+			// fprintf(stderr, "++++++ ? = %s\n", my_getenv(g_mshell->envlist, variable_name));
 			expanded_value = ft_strdup(my_getenv(g_mshell->envlist, variable_name));
-			printf("handle_token_in_quotes 56: variable_name = %s, str[i] = %c\n",  variable_name, str[i]);
+			// printf("handle_token_in_quotes 56: variable_name = %s, str[i] = %c\n",  variable_name, str[i]);
 			if (expanded_value == NULL && errno == 0)
 				expanded_value = ft_strdup("");
-			printf("handle_token_in_quotes 40: expanded_value = %s\n", expanded_value);
+			// printf("handle_token_in_quotes 40: expanded_value = %s\n", expanded_value);
 			expanded_value = ft_strjoin_with_free_no_null(strdup_before_valiable(t_str, variable_name), expanded_value);
-			printf("handle_token_in_quotes 42: expanded_value = %s\n", expanded_value);
+			// printf("handle_token_in_quotes 42: expanded_value = %s\n", expanded_value);
 			i += ft_strlen(variable_name);
 			free(variable_name);
 			free(t_str);
 			t_str = ft_strjoin_with_free_no_null(expanded_value, ft_substr(str+ i + 1, 0, ft_strlen(str+ i + 1)));
 			if (t_str == NULL)
 				error("handle_token_in_quotes.c 84: malloc error");
-			printf("handle_token_in_quotes 54: t_str = %s, i = %zu\n", t_str, i);
+			// printf("handle_token_in_quotes 54: t_str = %s, i = %zu\n", t_str, i);
 //			printf("handle_token_in_quotes 54: t_str = %s, i = %d, t_str[i] = %c\n", t_str, i, t_str[i]);
 //			continue;
 		}
@@ -153,12 +153,12 @@ size_t	replace_token(t_token **token, char *str)
  {
  	char	*expanded_value;
 
- 	 fprintf(stderr,"expand_token called\n");
+ 	fprintf(stderr,"expand_token called\n");
  	if (op_kind == TK_OP_DOLLAR)
  	{
  		expanded_value = expand_str((*token)->str);
-		 fprintf(stderr,"expand_token 173:  %s\n", expanded_value);
-		 fprintf(stderr,"expand_token 174:  %s\n", (*token)->next->str);
+		// fprintf(stderr,"expand_token 173:  %s\n", expanded_value);
+		// fprintf(stderr,"expand_token 174:  %s\n", (*token)->next->str);
  		if (errno)
  			error(strerror(errno));
  		return (replace_token(token, expanded_value));
