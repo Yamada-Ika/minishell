a069f59d941a2c70a4852d5b93225655666694d6
shige <shigebask@gmail.com>
2022-01-24 01:46:41 +0900

    fix: refactor expansion_utils.c

--- src/expander/expansion_utils.c
+++ src/expander/expansion_utils.c
@@ -3,6 +3,7 @@
 void	join_token_and_token_prev(t_token **token)
 {
 	t_token	*tmp;
+
 	printf("join_token_and_token_perv called\n");
 	(*token)->prev->str = ft_strjoin_with_free(&(*token)->prev->str, &(*token)->str);
 	if ((*token)->prev->str == NULL)
@@ -12,7 +13,6 @@ void	join_token_and_token_prev(t_token **token)
 	(*token)->next->prev = (*token)->prev;
 	printf("join_token_and_token_perv 12: str == %s\n", (*token)->prev->str);
 	tmp = *token;
-	//	*token =
 	*token = (*token)->prev;
 	free(tmp);
 }
@@ -21,12 +21,14 @@ char	*strdup_before_valiable(char *tmp, char *variable_name)
 {
 	size_t	i;
 	size_t	dif;
+	size_t	variable_name_len;
 
 	i = 0;
+	variable_name_len = ft_strlen(variable_name);
 	while (tmp[i])
 	{
 		if (tmp[i] == '$')
-			dif = ft_strncmp(tmp + i + 1, variable_name, ft_strlen(variable_name));
+			dif = ft_strncmp(tmp + i + 1, variable_name, variable_name_len);
 		if (dif == 0)
 			return (ft_substr(tmp, 0, i));
 		i++;
@@ -43,36 +45,23 @@ char	*expand_str(char *str)
 
 	i = 0;
 	t_str = ft_strdup(str);
-	// printf("strdup : t_str = %s\n", t_str);
 	while (str[i])
 	{
-		// printf("handle_token_in_quotes 52: i = %zu,   str[i] = %c\n", i, str[i]);
-		if (str[i] == '$' && (ft_isalnum((unsigned char)(str[i + 1])) || str[i + 1] == '?'))
+		if (str[i] == '$' && (ft_isalnum((str[i + 1])) || str[i + 1] == '?'))
 		{
 			variable_name = ft_substr(str + i, 1, get_valiable_name_len(str + i + 1));
-			// fprintf(stderr, "++++++ variable_name = %s\n", variable_name);
-			// fprintf(stderr, "++++++ ? = %s\n", my_getenv(g_mshell->envlist, variable_name));
 			expanded_value = ft_strdup(my_getenv(g_mshell->envlist, variable_name));
-			// printf("handle_token_in_quotes 56: variable_name = %s, str[i] = %c\n",  variable_name, str[i]);
 			if (expanded_value == NULL && errno == 0)
 				expanded_value = ft_strdup("");
-			// printf("handle_token_in_quotes 40: expanded_value = %s\n", expanded_value);
 			expanded_value = ft_strjoin_with_free_no_null(strdup_before_valiable(t_str, variable_name), expanded_value);
-			// printf("handle_token_in_quotes 42: expanded_value = %s\n", expanded_value);
 			i += ft_strlen(variable_name);
-			free(variable_name);
-			free(t_str);
-			t_str = ft_strjoin_with_free_no_null(expanded_value, ft_substr(str+ i + 1, 0, ft_strlen(str+ i + 1)));
+			ft_free((void **)&variable_name, (void **)&t_str);
+			t_str = ft_strjoin_with_free_no_null(expanded_value, ft_substr(str + i + 1, 0, ft_strlen(str + i + 1)));
 			if (t_str == NULL)
 				error("handle_token_in_quotes.c 84: malloc error");
-			// printf("handle_token_in_quotes 54: t_str = %s, i = %zu\n", t_str, i);
-			//			printf("handle_token_in_quotes 54: t_str = %s, i = %d, t_str[i] = %c\n", t_str, i, t_str[i]);
-			//			continue;
 		}
 		i++;
 	}
-	//	free(str);
-	//	str = t_str;
 	return (t_str);
 }
 
