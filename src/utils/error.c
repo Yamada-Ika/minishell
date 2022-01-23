0330fd6e87572cdc04c3cc44f9172530023a1f46
shige <shigebask@gmail.com>
2022-01-24 02:24:40 +0900

    fix: add move tokenize_error to error.c

--- src/utils/error.c
+++ src/utils/error.c
@@ -8,6 +8,19 @@ t_node	*parse_error(t_node *node, t_token *token)
 	return (NULL);
 }
 
+t_token	*tokenize_error(t_token *token)
+{
+	t_token *token_next;
+	while (token != NULL)
+	{
+		free (token->str);
+		token_next = token->next;
+		free (token);
+		token = token_next;
+	}
+	return (NULL);
+}
+
 void	error(char *str)
 {
 	ft_putstr_fd(str, 2);
