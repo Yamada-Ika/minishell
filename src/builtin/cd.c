c9b78a6e689a1b92e82421ad7a507a55c9bdbe4d
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-25 04:18:22 +0900

    fix: error message

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -29,8 +29,10 @@ void	cd_(char **cmds)
 	if (chdir(cmds[1]) == -1)
 	{
 		free(path);
-		ft_putstr_fd("cd: no such file or directory: ", STDERR_FILENO);
-		ft_putendl_fd(cmds[1], STDERR_FILENO);
+		ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
+		ft_putstr_fd(cmds[1], STDERR_FILENO);
+		ft_putstr_fd(": ", STDERR_FILENO);
+		ft_putendl_fd(strerror(errno), STDERR_FILENO);
 		return (add_exit_status_to_env(1));
 	}
 	_update_oldpwd_in_envlist(path);
