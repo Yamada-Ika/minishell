e1b5fa509832358cd7944deb72bdad89f3d37bb6
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-25 04:15:04 +0900

    fix: cd error case

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -1,37 +1,42 @@
 #include "minishell.h"
 
-static void	_update_pwd_helper(char *key)
+static void	_update_pwd_helper(char *path, char *key)
 {
-	char	*path;
 	char	*val;
 
 	val = my_getenv(g_mshell->envlist, key);
 	if (val == NULL)
 		return ;
-	path = getcwd(NULL, 0);
 	if (path != NULL)
 		msh_export(&(g_mshell->envlist), key, path);
-	free(path);
 }
 
-static void	_update_pwd_in_envlist(void)
+static void	_update_pwd_in_envlist(char *path)
 {
-	_update_pwd_helper("PWD");
+	_update_pwd_helper(path, "PWD");
 }
 
-static void	_update_oldpwd_in_envlist(void)
+static void	_update_oldpwd_in_envlist(char *path)
 {
-	_update_pwd_helper("OLDPWD");
+	_update_pwd_helper(path, "OLDPWD");
 }
 
 void	cd_(char **cmds)
 {
-	int	exit_status;
+	char	*path;
 
-	_update_oldpwd_in_envlist();
-	exit_status = 0;
+	path = getcwd(NULL, 0);
 	if (chdir(cmds[1]) == -1)
-		exit_status = 1;
-	add_exit_status_to_env(exit_status);
-	_update_pwd_in_envlist();
+	{
+		free(path);
+		ft_putstr_fd("cd: no such file or directory: ", STDERR_FILENO);
+		ft_putendl_fd(cmds[1], STDERR_FILENO);
+		return (add_exit_status_to_env(1));
+	}
+	_update_oldpwd_in_envlist(path);
+	free(path);
+	add_exit_status_to_env(0);
+	path = getcwd(NULL, 0);
+	_update_pwd_in_envlist(path);
+	free(path);
 }
