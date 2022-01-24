c9b7628106e20b233898dd8be149df5624c1eb85
shige <shigebask@gmail.com>
2022-01-24 15:02:29 +0900

    fix: split func expand_node

--- src/expander/expansion.c
+++ src/expander/expansion.c
@@ -1,32 +1,5 @@
 #include "minishell.h"
 
-static size_t	join_valiable(char **p, t_token **tok)
-{
-	t_token	*cur;
-	t_token	head;
-	size_t	count;
-
-	count = 0;
-	head.next = NULL;
-	cur = &head;
-	while (p[count])
-	{
-		cur->next = new_token_tk_word(TK_WORD, p[count], ft_strlen(p[count]));
-		cur->next->prev = cur;
-		cur = cur->next;
-		count++;
-	}
-	free(p);
-	cur->next = (*tok)->next;
-	cur->next->prev = cur;
-	(*tok)->prev->next = head.next;
-	head.next->prev = (*tok)->prev;
-	free((*tok)->str);
-	free(*tok);
-	*tok = cur;
-	return (count);
-}
-
 size_t	replace_token(t_token **token, char *str)
 {
 	size_t	increase_tok_num;
@@ -72,16 +45,13 @@ size_t	expand_token(t_token **token, int op_kind)
 	return (0);
 }
 
-void	expand_node(t_node *node)
+void	expand_node_word_list(t_node *node)
 {
-	t_token	*head;
 	size_t	cur_index;
 	size_t	added_token_size;
 	int		op_kind;
 
-	head = node->word_list->prev;
 	cur_index = 0;
-	fprintf(stderr, "word_list_size : %zu cur_index : %zu\n", node->word_list_size, cur_index);
 	while (cur_index++ < node->word_list_size)
 	{
 		op_kind = node->word_list->kind;
@@ -101,6 +71,14 @@ void	expand_node(t_node *node)
 		}
 		node->word_list = node->word_list->next;
 	}
+}
+
+void	expand_node(t_node *node)
+{
+	t_token	*head;
+
+	head = node->word_list->prev;
+	expand_node_word_list(node);
 	node->word_list = head->next;
 	create_t_command(node);
 }
