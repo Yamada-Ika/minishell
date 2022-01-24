24cd44beca4bf2835ee14ff27cb690f68b9b1745
shige <shigebask@gmail.com>
2022-01-25 01:30:30 +0900

    fix: add func get_exit_status

--- src/utils/set_exit_status.c
+++ src/utils/set_exit_status.c
@@ -27,3 +27,18 @@ void	add_exit_status_to_env(int status)
 	msh_export(&(g_mshell->envlist), "?", status_str);
 	free(status_str);
 }
+
+int	get_exit_status(int status)
+{
+	if (WIFSIGNALED(status))
+	{
+		status = 128 + WTERMSIG(status);
+		//		printf("signal exit status %d\n", status);
+	}
+	else
+	{
+		//		printf("other exit status %d\n", WEXITSTATUS(status));
+		status = WEXITSTATUS(status);
+	}
+	return (status);
+}
