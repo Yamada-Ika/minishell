1546bc77c42eb794b506920bcf55c04ef44f5436
shige <shigebask@gmail.com>
2022-01-24 21:50:35 +0900

    fix: stop message in set_exit_status.c

--- src/utils/set_exit_status.c
+++ src/utils/set_exit_status.c
@@ -7,11 +7,11 @@ void	set_exit_status(int status)
 	if (WIFSIGNALED(status))
 	{
 		status = 128 + WTERMSIG(status);
-		printf("signal exit status %d\n", status);
+//		printf("signal exit status %d\n", status);
 	}
 	else
 	{
-		printf("other exit status %d\n", WEXITSTATUS(status));
+//		printf("other exit status %d\n", WEXITSTATUS(status));
 		status = WEXITSTATUS(status);
 	}
 	add_exit_status_to_env(status);
