b5ab096c23b1f1a37cf2d5b19385b7cc9dabf19c
shige <shigebask@gmail.com>
2022-01-24 17:15:46 +0900

    add: utils2.c

---
+++ src/utils/utils2.c
@@ -0,0 +1,32 @@
+#include "minishell.h"
+
+void	ft_dup2(int oldfd, int newfd)
+{
+	if (dup2(oldfd, newfd) == ERROR)
+		error(strerror(errno));
+}
+
+int	ft_dup(int fd)
+{
+	int	duplicated_fd;
+	duplicated_fd = dup(fd);
+	if (duplicated_fd == ERROR)
+		error(strerror(errno));
+	return (duplicated_fd);
+}
+
+void	ft_pipe(int *fd)
+{
+	if (pipe(fd) == ERROR)
+		error(strerror(errno));
+}
+
+pid_t	ft_fork()
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == ERROR)
+		error(strerror(errno));
+	return (pid);
+}
