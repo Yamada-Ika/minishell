68ebe508e753ef5d6b12c4a3478affed01155ca2
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-25 17:14:31 +0900

    delete: comment

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -2,17 +2,6 @@
 
 void	pwd_(void)
 {
-	// int		exit_status;
-	// char	*path;
-
 	printf("%s\n", g_mshell->pwd);
-	// if ()
-	// exit_status = 0;
-	// path = getcwd(NULL, 0);
-	// if (path == NULL)
-	// 	exit_status = 1;
-	// if (path != NULL)
-	// 	printf("%s\n", path);
-	// add_exit_status_to_env(exit_status);
-	// free(path);
+	add_exit_status_to_env(0);
 }
