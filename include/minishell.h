011a6314b43fbeece61b9fcd6ee78a671801c969
shige <shigebask@gmail.com>
2022-01-27 00:49:54 +0900

    fix: add exec_cmd

--- include/minishell.h
+++ include/minishell.h
@@ -15,6 +15,7 @@
 //#include "../test/debug.h"
 # include <stdbool.h>
 # include <dirent.h>
+# include <sys/stat.h>
 
 // env
 # include "envvar.h"
@@ -217,7 +218,9 @@ void			_exec_builtin_cmd(t_builtin_kind kind, char **cmds);
 char			**get_command_path(t_envvar *envlist);
 char			**_alloc_word_list(t_token *tok, size_t size);
 void			handle_fd(int close_fd, int dup_fd, int fd);
-bool			is_exec_cmd_with_full_path(char **cmds, char **environ);;
+void			exec_cmd_with_full_path(char **cmds, char **environ);
+void			exec_cmd(char *path, char **cmds, char **environ);
+
 
 // signal.c
 void			display_new_prompt(int signal);
