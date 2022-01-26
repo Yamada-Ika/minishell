07f78e5f142243924fc40a15408b7c7b7d4e66e1
shige <shigebask@gmail.com>
2022-01-26 17:39:58 +0900

    fix:

--- test2/main_for_test.c
+++ test2/main_for_test.c
@@ -6,13 +6,30 @@ static void	_install_signal_handler(void)
 	signal(SIGQUIT, SIG_IGN);
 }
 
-static void	_init_global_var()
+static void	_set_pwd(void)
+{
+	char	*pwd_path;
+
+	pwd_path = getcwd(NULL, 0);
+	if (pwd_path == NULL)
+	{
+		ft_putstr_fd("shell-init: error retrieving current directory: \
+getcwd: cannot access parent directories: ", STDERR_FILENO);
+		ft_putendl_fd(strerror(errno), STDERR_FILENO);
+		exit(0);
+	}
+	g_mshell.pwd = pwd_path;
+}
+
+static	void	_init_global_var(void)
 {
 	extern char	**environ;
 
-	g_mshell = ft_calloc(1, sizeof(g_mshell));
+	//	g_mshell = ft_calloc(1, sizeof(g_mshell));
 	inherite_env_val(&(g_mshell.envlist), environ);
+	set_exit_status(0);
 	g_mshell.interrupt = false;
+	_set_pwd();
 }
 
 static void	_frees(char **strs)
