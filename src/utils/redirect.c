54e8dc82f2d7c45841bd375a9505f747ea713818
shige <shigebask@gmail.com>
2022-01-26 23:43:03 +0900

    fix: use error_message

--- src/utils/redirect.c
+++ src/utils/redirect.c
@@ -8,9 +8,7 @@ int	handle_in_redir(t_redirect_list *redirect)
 		return (0);
 	if (redirect->is_ambiguous == true)
 	{
-		ft_putstr_fd("minishell: ", 2);
-		ft_putstr_fd(redirect->word, 2);
-		ft_putendl_fd(": ambiguous redirect", 2);
+		error_message(redirect->word, "ambiguous redirect" );
 		return (-1);
 	}
 	if (ft_strncmp(redirect->redirect, ">>", 2) == 0)
@@ -19,7 +17,8 @@ int	handle_in_redir(t_redirect_list *redirect)
 		fd = open(redirect->word, O_RDWR | O_CREAT | O_TRUNC, 0664);
 	if (fd == -1)
 	{
-		ft_putendl_fd(strerror(errno), 2);
+		error_message(redirect->word, strerror(errno) );
+//		ft_putendl_fd(strerror(errno), 2);
 		return (-1);
 	}
 	ft_dup2(fd, 1);
@@ -37,9 +36,7 @@ int	handle_out_redir(t_redirect_list *redirect)
 		return (0);
 	if (redirect->is_ambiguous == true)
 	{
-		ft_putstr_fd("minishell: ", 2);
-		ft_putstr_fd(redirect->word, 2);
-		ft_putendl_fd(": ambiguous redirect", 2);
+		error_message(redirect->word, "ambiguous redirect" );
 		return (-1);
 	}
 	if (ft_strncmp(redirect->redirect, "<<", 2))
@@ -47,7 +44,8 @@ int	handle_out_redir(t_redirect_list *redirect)
 		fd = open(redirect->word, O_RDONLY);
 		if (fd == -1)
 		{
-			ft_putendl_fd(strerror(errno), 2);
+			error_message(redirect->word, strerror(errno) );
+//			ft_putendl_fd(strerror(errno), 2);
 			return (-1);
 		}
 		ft_dup2(fd, 0);
