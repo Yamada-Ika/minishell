820e7c31e6657d19839a10fc8ddbae97351b937d
shige <shigebask@gmail.com>
2022-01-27 02:39:51 +0900

    fix: add is_str_all_c

--- libft/libft.h
+++ libft/libft.h
@@ -81,5 +81,6 @@ char	*strjoin_and_free(char *s1, char *s2);
 char	***ft_split_triple(char const **strs, char *sets);
 int		ft_strcmp(const char*s1, const char *s2);
 void	ft_free(void **p1, void **p2);
+bool	is_str_all_c(char *str, char c);
 
 #endif
