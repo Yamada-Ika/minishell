443dcb315051e87a35f0d98e684247a4a1f5a306
shige <shigebask@gmail.com>
2022-01-27 23:47:38 +0900

    fix: move func to tokennize_utils.c

--- src/lexar/tokenize_utils.c
+++ src/lexar/tokenize_utils.c
@@ -14,3 +14,18 @@ bool	is_quote_closed(char c, t_token_kind kind)
 		return (true);
 	return (false);
 }
+
+int	check_op(char *s)
+{
+	const char	*kw[] = {">>", "<<", "<", ">", "|", "'", "\"", "$", NULL};
+	int			i;
+
+	i = 0;
+	while (kw[i] != NULL)
+	{
+		if (!ft_strncmp(s, kw[i], ft_strlen(kw[i])))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
