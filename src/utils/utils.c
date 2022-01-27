1c55228a2a453093a6ebceeea4f5dff6a59330a7
shige <shigebask@gmail.com>
2022-01-27 23:36:41 +0900

    fix: add funcs

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,47 +1,8 @@
 #include "minishell.h"
 
-int	check_op(char *s)
+bool	can_c_be_env_name(char c)
 {
-	const char	*kw[] = {">>", "<<", "<", ">", "|", "'", "\"", "$", NULL};
-	int			i;
-
-	i = 0;
-	while (kw[i] != NULL)
-	{
-		if (!ft_strncmp(s, kw[i], ft_strlen(kw[i])))
-			return (i);
-		i++;
-	}
-	return (-1);
-}
-
-size_t	get_word_len(char *p, t_token_kind kind, char *str)
-{
-	size_t	len;
-	int		quote_count;
-	bool	is_dollar;
-
-	len = 0;
-	quote_count = 0;
-	is_dollar = false;
-	while (kind != TK_WORD || !ft_strchr(str, p[len]))
-	{
-		if (kind == TK_OP_DOLLAR && p[len] == '$'
-			&& (ft_isalnum(p[len + 1]) || p[len + 1] == '?'))
-			is_dollar = true;
-		else if (is_dollar && (!ft_isalnum(p[len]) || p[len] == '?'))
-			break ;
-		if (p[len] == '\'' && kind == TK_WORD_IN_SINGLE_Q)
-			quote_count += 1;
-		if (p[len] == '"' && kind == TK_WORD_IN_DOUBLE_Q)
-			quote_count += 1;
-		if (quote_count == 2)
-			kind = TK_WORD;
-		if (kind == TK_WORD && p[len] == '$' && ft_isalnum(p[len + 1]))
-			break ;
-		len++;
-	}
-	return (len);
+	return (ft_isalnum(c) || c == '_');
 }
 
 size_t	get_env_name_len(char *str)
@@ -49,9 +10,48 @@ size_t	get_env_name_len(char *str)
 	size_t	i;
 
 	i = 0;
-	while (ft_isalnum((char) str[i]) || str[i] == '?')
+	if (*str == '?')
+		return (1);
+	while (str[i])
+	{
+		if (can_c_be_env_name(str[i]) == false)
+			return (i);
 		i++;
+	}
 	return (i);
+}
+
+bool	is_env_name(char *str)
+{
+	if (*str != '$')
+		return (false);
+	if (str[1] == '?' || can_c_be_env_name(str[1]))
+		return (true);
+	return (false);
+}
+
+size_t	get_word_len(char *p, t_token_kind kind, char *str)
+{
+	size_t	len;
+	int		quote_count;
+
+	len = 0;
+	quote_count = 0;
+	while (kind != TK_WORD || !ft_strchr(str, p[len]))
+	{
+		if (kind == TK_WORD && is_env_name(p + len))
+			break ;
+		if (kind == TK_OP_DOLLAR && is_env_name(p + len))
+			return (len + 1 + get_env_name_len(p + len + 1));
+		if (p[len] == '\'' && kind == TK_WORD_IN_SINGLE_Q)
+			quote_count += 1;
+		if (p[len] == '"' && kind == TK_WORD_IN_DOUBLE_Q)
+			quote_count += 1;
+		if (quote_count == 2)
+			kind = TK_WORD;
+		len++;
+	}
+	return (len);
 }
 
 bool	is_redirect_kind(t_token_kind kind)
@@ -61,13 +61,4 @@ bool	is_redirect_kind(t_token_kind kind)
 		|| kind == TK_OP_LS
 		|| kind == TK_OP_GR
 	);
-}
-
-t_redirect_list	*_redir_lstlast(t_redirect_list *lst)
-{
-	if (!lst)
-		return (NULL);
-	while (lst->next)
-		lst = lst->next;
-	return (lst);
 }
